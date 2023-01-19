d7c15824b038668ec6336694edb835a81f061640
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-01-19 21:03:54 +0600

    first commit

---
+++ Basic-JavaScript-Problem-Solving/prblm-5(return first value of an array).js
@@ -0,0 +1,13 @@
+// Problem : Write Function to return first value of an array
+function getFirstValue(arr){
+  //Write Your solution Here
+}
+
+console.log(getFirstValue(["Cricket", "Football", "WWE"])); //Cricket
+console.log(getFirstValue(["Shakib", "Neymar", "Ronaldo"])); //Shakib
+console.log(getFirstValue([75, 10, 7])); //75
+
+// Solution is here
+function getFirstValue(arr){
+  return arr[0];
+}
