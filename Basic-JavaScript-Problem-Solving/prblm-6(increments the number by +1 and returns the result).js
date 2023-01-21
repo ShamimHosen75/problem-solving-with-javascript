a6c6a2625d4264a66f07b7155a653f05f1ae9963
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-01-21 23:32:49 +0600

    first commit

---
+++ Basic-JavaScript-Problem-Solving/prblm-6(increments the number by +1 and returns the result).js
@@ -0,0 +1,14 @@
+// Problem : Create a function that takes a number as an argument, increments the number by +1 and returns the result
+function addition(num){
+  //Write Your solution Here
+}
+
+console.log(addition(5)); //6
+console.log(addition(75)); //76
+console.log(addition(99)); //100
+
+// Solution is here
+function addition(num){
+  let numPlusOne = num + 1;
+  return(numPlusOne);
+}
