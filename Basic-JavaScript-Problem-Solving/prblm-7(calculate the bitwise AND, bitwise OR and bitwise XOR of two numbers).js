cbde887b50f3de3967ac8e62d1591076e9b0f473
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-02-01 03:50:58 +0600

    first commit

---
+++ Basic-JavaScript-Problem-Solving/prblm-7(calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers).js
@@ -0,0 +1,35 @@
+// Problem : Write functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers
+
+function bitwiseAND(n1, n2) {
+  //Write Your solution Here
+};
+
+function bitwiseOR(n1, n2) {
+  //Write Your solution Here
+};
+
+function bitwiseXOR(n1, n2) {
+  //Write Your solution Here
+};
+
+console.log(bitwiseAND(10, 20)); // 0
+console.log(bitwiseOR(10, 20)); // 30
+console.log(bitwiseXOR(10, 20)); // 30
+
+
+
+// Solution is here
+function bitwiseAND(n1, n2) {
+  let answer = n1 & n2;
+  return (answer);
+};
+
+function bitwiseOR(n1, n2) {
+  let answer = n1 | n2;
+  return (answer);
+};
+
+function bitwiseXOR(n1, n2) {
+  let answer = n1 ^ n2;
+  return (answer);
+};
