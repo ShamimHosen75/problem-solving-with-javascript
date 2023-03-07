3bbf1e62864b8d5e46c366d9e11bd9967aec041c
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-03-07 23:51:52 +0600

    commit solve the problem

---
+++ Basic-JavaScript-Problem-Solving/prblm-9(return true when num1 is equal to num2 otherwise return false).js
@@ -0,0 +1,19 @@
+// Problem : Create a function that returns true when num1 is equal to num2; otherwise return false
+function isSameNum(num1, num2){
+  // Write Your solution Here
+};
+
+console.log(isSameNum(30, 30)); // true
+console.log(isSameNum(20, 40)); // false
+console.log(isSameNum(50, 50)); // true
+
+
+// Solution is here
+function isSameNum(num1, num2){
+  if(num1 == num2){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
